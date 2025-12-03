26c8da02c303bfe511cf73cbccc9caa2cf896ca8
Unname <muhamadimambaehaqi@gmail.com>
2025-12-03 11:11:56 +0700

    update gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M88WJ5KJ');`
+})(window,document,'script','dataLayer','GTM-KLTDQPG5');`
         }} />
         {/* End Google Tag Manager */}
 
@@ -90,7 +90,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M88WJ5KJ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KLTDQPG5"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
